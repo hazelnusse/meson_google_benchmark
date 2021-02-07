3e8e16498305514de61b907b0e327865c0751494
Luke Peterson <hazelnusse@gmail.com>
2021-02-06 19:20:31 -0800

    Initial commit

---
+++ mwe.cpp
@@ -0,0 +1,31 @@
+// This file is taken from the basic usage example here:
+// https://github.com/google/benchmark#basic-usage
+#include <benchmark/benchmark.h>
+
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state)
+    std::string empty_string;
+}
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+static void BM_StringCompare(benchmark::State& state) {
+  std::string s1(state.range(0), '-');
+  std::string s2(state.range(0), '-');
+  for (auto _ : state) {
+      benchmark::DoNotOptimize(s1.compare(s2));
+  }
+  state.SetComplexityN(state.range(0));
+}
+BENCHMARK(BM_StringCompare)
+        ->RangeMultiplier(2)->Range(1<<10, 1<<18)->Complexity(benchmark::oN);
+
+BENCHMARK_MAIN();
